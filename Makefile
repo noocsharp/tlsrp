c73f560b91e502dff181ab6338693310e66cab56
Nihal Jere <nihal@nihaljere.xyz>
2020-07-30 17:46:31 -0500

    Makefile: add install and uninstall targets

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 include config.mk
 
+SRC = tlsrp.c util.c
+OBJ = $(SRC:.c=.o)
+
 all: config.h tlsrp
 
 config.h:
 	cp config.def.h $@
 
 .c.o:
-	$(CC) $(CFLAGS) -c $<
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $<
 
 tlsrp: $(OBJ) config.h
 	$(CC) $(FLAGS) $(OBJ) -o $@
@@ -16,6 +19,18 @@ certs:
 
 clean:
 	rm -f $(OBJ) tlsrp
+
+install: all
+	mkdir -p "$(DESTDIR)$(PREFIX)/bin"
+	cp -f tlsrp "$(DESTDIR)$(PREFIX)/bin"
+	chmod 755 "$(DESTDIR)$(PREFIX)/bin/tlsrp"
+	mkdir -p "$(DESTDIR)$(MANPREFIX)/man1"
+	cp tlsrp.1 "$(DESTDIR)$(MANPREFIX)/man1/tlsrp.1"
+	chmod 644 "$(DESTDIR)$(MANPREFIX)/man1/tlsrp.1"
+
+uninstall:
+	rm -f "$(DESTDIR)$(PREFIX)/bin/tlsrp"
+	rm -f "$(DESTDIR)$(MANPREFIX)/man1/tlsrp.1"
 
 test: tlsrp certs
 	LD_LIBRARY_PATH=$(LIB_PATH) ./tlsrp -u "/tmp/conn.socket" -P 8000 -a "CA/root.crt" -r "CA/tlsrp.crt" -k "CA/tlsrp.key"
--- config.mk
+++ config.mk
@@ -1,17 +1,19 @@
+# flags
 CC = cc
 CFLAGS = -std=c99 -Wall -Wextra
 
-# Arch Linux Testing
-LIB_PATH=/lib/libressl
-LIBTLS_PKGCONF_PATH = /usr/lib/libressl/pkgconfig/
-CFLAGS := $(CFLAGS) `PKG_CONFIG_PATH=$(LIBTLS_PKGCONF_PATH) pkg-config --cflags libtls`
-FLAGS = `PKG_CONFIG_PATH=$(LIBTLS_PKGCONF_PATH) pkg-config --libs libtls`
-
 # glibc
-CFLAGS := $(CFLAGS) -D_XOPEN_SOURCE=700
+CPPFLAGS = -D_XOPEN_SOURCE=700
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+# Arch Linux Testing
+# LIB_PATH=/lib/libressl
+# LIBTLS_PKGCONF_PATH = /usr/lib/libressl/pkgconfig/
+# CFLAGS := $(CFLAGS) `PKG_CONFIG_PATH=$(LIBTLS_PKGCONF_PATH) pkg-config --cflags libtls`
+# FLAGS = `PKG_CONFIG_PATH=$(LIBTLS_PKGCONF_PATH) pkg-config --libs libtls`
 
 # Any system that is using LibreSSL as the default SSL provider
-# FLAGS := $(FLAGS) -ltls
-
-SRC = tlsrp.c util.c
-OBJ = $(SRC:.c=.o)
+FLAGS = -ltls
